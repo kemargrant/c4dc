20bf658d7790227da196ba44651eb1eb721360a3
chris grant <christophe.grant@gmail.com>
2018-05-29 01:40:09 -0400

    Remove default points from movingLine

--- src/App.js
+++ src/App.js
@@ -698,45 +698,53 @@ const CustomTable = function(props){
 }
 
 const MovingLine = function(props) {
-	return(<Scatter
-				data={
-				{labels: ['Scatter'],
-					datasets:[
-				    {
-				      label: 'Percentage Moving Average ('+ props.gauge[0][props.gauge[0].length-1].y+')',
-				      borderColor:"red",
-				      data: props.gauge[0],
-				      showLine:true,
-				      lineTension:0,
-				      radius:0,
-				      fill:false,
-				    },
-				    {
-				      label: 'Percent('+props.gauge[1][props.gauge[1].length-1].y +')',
-				      borderColor:"blue",
-				      backgroundColor:"black",
-				      radius:0,
-				      data: props.gauge[1],
-					  showLine:true,
-					  lineTension:0,
-				      fill:false,
-				    }
-				  ]
-				}}
-			height={props.height}
-			options={{
-			events:[],	
-			animation:{duration:10},	
-			scales: {
-				xAxes: [{
-					type: 'linear',
-					ticks:{
-						stepSize:10
-					}
-				}],	
-			},
-		}} 
-		/>)
+	var render;
+	switch (props.gauge[0][0]){
+		case (undefined):
+			render = <Scatter data={{}} height={props.height}/>
+			break
+		default:
+		render = <Scatter
+					data={
+					{labels: ['Scatter'],
+						datasets:[
+					    {
+					      label: 'Percentage Moving Average ('+ props.gauge[0][props.gauge[0].length-1].y+')',
+					      borderColor:"red",
+					      data: props.gauge[0],
+					      showLine:true,
+					      lineTension:0,
+					      radius:0,
+					      fill:false,
+					    },
+					    {
+					      label: 'Percent('+props.gauge[1][props.gauge[1].length-1].y +')',
+					      borderColor:"blue",
+					      backgroundColor:"black",
+					      radius:0,
+					      data: props.gauge[1],
+						  showLine:true,
+						  lineTension:0,
+					      fill:false,
+					    }
+					  ]
+					}}
+				height={props.height}
+				options={{
+				events:[],	
+				animation:{duration:10},	
+				scales: {
+					xAxes: [{
+						type: 'linear',
+						ticks:{
+							stepSize:10
+						}
+					}],	
+				},
+			}} 
+			/>
+	}
+	return render;
 }
 
 class BittrexState extends React.Component{
@@ -1067,8 +1075,8 @@ class App extends Component{
 			binanceB1Minimum:{},
 			binanceC1Minimum:{},
 			binanceConnections:0,
-			binanceGauge:[[{x:0,y:0}],[{x:0,y:0}]],
-			bittrexGauge:[[{x:0,y:0}],[{x:0,y:0}]],
+			binanceGauge:[[],[]],
+			bittrexGauge:[[],[]],
 			binanceLimits:{},
 			binanceOptimalTrades:{},
 			binancePairs:[],
