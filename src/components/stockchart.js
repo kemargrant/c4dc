b51e3ba75bd9e6309e59183cbb12a22de5c92c11
kemargrant <kemargrant@gmail.com>
2018-11-12 00:01:11 -0500

    Replace react-chartjs with chartkick

--- src/components/stockchart.js
+++ src/components/stockchart.js
@@ -1,5 +1,8 @@
+import Chart from 'chart.js'
 import React from 'react';
-import {Scatter} from 'react-chartjs-2';
+import ReactChartkick, {AreaChart} from 'react-chartkick'
+
+ReactChartkick.addAdapter(Chart)
 
 //Bittrex Components
 function StockChart(props){
@@ -60,30 +63,33 @@ function StockChart(props){
 	if(props.data["Sorted"]){
 		if(!props.small){
 			config.data.datasets[0].data = props.data["Sorted"] && props.data["Sorted"][1].map((order) => {
-				return {y:props.data["Bids"][order],x:order}
+				return [order,props.data["Bids"][order]]
 			})
 			config.data.datasets[1].data = props.data["Sorted"] && props.data["Sorted"][0].map((order) => {
-				return {y:props.data["Asks"][order],x:order}
+				return [order,props.data["Asks"][order]]
 			})
 			config.options.title.text = props.pair;
-			config.options.scales.xAxes[0].ticks.max = Number(props.data["Sorted"][0][props.data["Sorted"][0].length - 1])
-			config.options.scales.xAxes[0].ticks.min = Number(props.data["Sorted"][1][props.data["Sorted"][1].length - 1])
+			//config.options.scales.xAxes[0].ticks.max = Number(props.data["Sorted"][0][props.data["Sorted"][0].length - 1])
+			//config.options.scales.xAxes[0].ticks.min = Number(props.data["Sorted"][1][props.data["Sorted"][1].length - 1])
 		}
 		else{
 			config.data.datasets[0].data = props.data["Sorted"] && props.data["Sorted"][1].map((order) => {
-				return {y:props.data["Bids"][order],x:order/props.data["Sorted"][1][props.data["Sorted"][1].length - 1]}
+				return [order/props.data["Sorted"][1][props.data["Sorted"][1].length - 1],props.data["Bids"][order]]
 			})
 			config.data.datasets[1].data = props.data["Sorted"] && props.data["Sorted"][0].map((order) => {
-				return {y:props.data["Asks"][order],x:order/props.data["Sorted"][1][props.data["Sorted"][1].length - 1]}
+				return [order/props.data["Sorted"][1][props.data["Sorted"][1].length - 1],props.data["Asks"][order]]
 			})
 			config.options.title.text = props.pair + "(Scaled)";
-			config.options.scales.xAxes[0].ticks.min = 1;
-			config.options.scales.xAxes[0].ticks.max = Number(props.data["Sorted"][0][props.data["Sorted"][1].length - 1])/Number(props.data["Sorted"][1][props.data["Sorted"][1].length - 1])
+			//config.options.scales.xAxes[0].ticks.min = 1;
+			//config.options.scales.xAxes[0].ticks.max = Number(props.data["Sorted"][0][props.data["Sorted"][1].length - 1])/Number(props.data["Sorted"][1][props.data["Sorted"][1].length - 1])
 		}
-		return <Scatter
-				height={props.style.height > 400 ? 50 : props.style.height/2}
-				data={config.data}
-				options={config.options} 
+		return <AreaChart
+				height={props.style.height > 400 ? "50px" : props.style.height+"px"}
+				data={[
+					{data:config.data.datasets[0].data,name:config.data.datasets[0].label},
+					{data:config.data.datasets[1].data,name:config.data.datasets[1].label}
+				]}
+				library={config.options} 
 				/>
 	}	
 	return null;
